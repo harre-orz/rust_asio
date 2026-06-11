a01526296c91927aed902d3736a7b526c43b88d8
Haruhiko Uchida <harre.orz@gmail.com>
2026-06-12 01:36:13 +0900

    up

--- src/poll/poll_iocp.rs
+++ src/poll/poll_iocp.rs
@@ -5,7 +5,119 @@ use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, Waker};
 use std::{mem, ptr};
 use windows_sys::Win32::Foundation;
+use windows_sys::Win32::Networking::WinSock;
 use windows_sys::Win32::System::IO;
+
+pub(crate) struct WinSockEx {
+    pub ConnectEx: unsafe fn(
+        s: WinSock::SOCKET,
+        name: *const WinSock::SOCKADDR,
+        namelen: i32,
+        lpsendbuffer: *const core::ffi::c_void,
+        dwsenddatalength: u32,
+        lpdwbytessent: *mut u32,
+        lpoverlapped: *mut IO::OVERLAPPED,
+    ) -> windows_sys::core::BOOL,
+    pub WSARecvMsg: unsafe fn(
+        s: WinSock::SOCKET,
+        lpmsg: *mut WinSock::WSAMSG,
+        lpdwnumberofbytesrecvd: *mut u32,
+        lpoverlapped: *mut IO::OVERLAPPED,
+        lpcompletionroutine: WinSock::LPWSAOVERLAPPED_COMPLETION_ROUTINE,
+    ) -> i32,
+}
+
+impl Drop for WinSockEx {
+    fn drop(&mut self) {
+        unsafe {
+            let _ = WinSock::WSACleanup();
+        }
+    }
+}
+
+struct SocketGuard(WinSock::SOCKET);
+
+impl Drop for SocketGuard {
+    fn drop(&mut self) {
+        unsafe {
+            WinSock::closesocket(self.0);
+        }
+    }
+}
+
+impl SocketGuard {
+    fn ex(&self) -> Result<WinSockEx> {
+        unsafe {
+            let ConnectEx = {
+                let guid = WinSock::WSAID_CONNECTEX;
+                let mut lpfn: WinSock::LPFN_CONNECTEX = None;
+                let mut bytes = 0;
+                match WinSock::WSAIoctl(
+                    self.0,
+                    WinSock::SIO_GET_EXTENSION_FUNCTION_POINTER,
+                    ptr::from_ref(&guid).cast(),
+                    size_of_val(&guid) as _,
+                    ptr::from_mut(&mut lpfn).cast(),
+                    size_of_val(&lpfn) as _,
+                    &mut bytes,
+                    ptr::null_mut(),
+                    None,
+                ) {
+                    WinSock::SOCKET_ERROR => return Err(OsError::last()),
+                    _ => lpfn.unwrap(),
+                }
+            };
+
+            let WSARecvMsg = {
+                let guid = WinSock::WSAID_WSARECVMSG;
+                let mut lpfn: WinSock::LPFN_WSARECVMSG = None;
+                let mut bytes = 0;
+                match WinSock::WSAIoctl(
+                    self.0,
+                    WinSock::SIO_GET_EXTENSION_FUNCTION_POINTER,
+                    ptr::from_ref(&guid).cast(),
+                    size_of_val(&guid) as _,
+                    ptr::from_mut(&mut lpfn).cast(),
+                    size_of_val(&lpfn) as _,
+                    &mut bytes,
+                    ptr::null_mut(),
+                    None,
+                ) {
+                    WinSock::SOCKET_ERROR => return Err(OsError::last()),
+                    _ => lpfn.unwrap(),
+                }
+            };
+            Ok(WinSockEx {
+                ConnectEx: ConnectEx,
+                WSARecvMsg: WSARecvMsg,
+            })
+        }
+    }
+}
+
+impl WinSockEx {
+    pub(crate) fn new() -> Result<Self> {
+        unsafe {
+            let mut _data = MaybeUninit::<WinSock::WSADATA>::uninit();
+            match WinSock::WSAStartup(0x0202, _data.as_mut_ptr()) {
+                0 => {
+                    match WinSock::WSASocketW(
+                        WinSock::AF_INET as i32,
+                        WinSock::SOCK_STREAM,
+                        WinSock::IPPROTO_IP,
+                        ptr::null_mut(),
+                        0,
+                        0,
+                    ) {
+                        WinSock::INVALID_SOCKET => Err(OsError::last()),
+                        soc => Ok(SocketGuard(soc).ex()?),
+                    }
+                }
+                err => Err(OsError::from_raw(err)),
+            }
+        }
+    }
+}
 
 enum EventOp {
     Neutral,
@@ -90,6 +202,7 @@ fn iocp_poll(iocp: &Handle, timeout: u32) -> Result<(Result<usize>, Event)> {
 }
 
 pub struct Iocp {
+    ex: WinSockEx,
     iocp: Handle,
     pub(super) intr: Intr,
     intr_event: Event,
@@ -97,11 +210,13 @@ pub struct Iocp {
 
 impl Iocp {
     pub(super) fn new() -> Result<Iocp> {
+        let ex = WinSockEx::new()?;
         let iocp = iocp_new()?;
         let intr = Intr::new()?;
         let intr_event: Event = Default::default();
         iocp_add(&iocp, intr.as_handle(), &intr_event);
         Ok(Iocp {
+            ex: ex,
             iocp: iocp,
             intr: intr,
             intr_event: intr_event,
