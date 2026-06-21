f638374ab6420daf0378c2ac07b3ffb0ffc52b51
Haruhiko Uchida <harre.orz@gmail.com>
2026-06-21 16:55:34 +0900

    up

--- src/core/intr/ktimer.rs
+++ src/core/intr/ktimer.rs
@@ -1,26 +1,11 @@
 use crate::error::OsError;
 
-pub(in super::super) struct Ktimer {
-
-}
+pub(in super::super) struct Ktimer;
 
 impl Ktimer {
-    pub fn new() -> Result<Self, OsError> {
-        Ok(Ktimer {})
+    pub const fn new() -> Result<Ktimer, OsError> {
+        Ok(Ktimer)
     }
 
-    pub fn timeout_kqueue(&self) -> libc::timespec {
-        libc::timespec {
-            tv_sec: 10000,
-            tv_nsec: 0,
-        }
-    }
-
-    pub fn wake_up_now(&self) {
-
-    }
-
-    pub fn update_event(&self) {
-
-    }
+    pub const fn update_event(&self) {}
 }
